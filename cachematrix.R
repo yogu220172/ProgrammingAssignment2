d6d8d1a8b7b1d9e3753ba5eb3e32235cb6cdf8b6
yogu220172 <yogu_n@yahoo.com>
2015-10-25 15:13:00 +0530

    Update cachematrix.R
    The assignment submitted by Santosh Narayanan, Signature track, e-mail id : yogu_n@yahoo.com

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Lexical Scoping Assignment 
+## This assignment demonstrates the use of caching effectively in
+## inverting matrices. The invserse of a matrix defined as 
+##   A^-1 = adj(A)/|A| where |A| != 0 
+## Using superassignment operators the given matrix & its inverse are 
+## made available in global environment
 
-## Write a short comment describing this function
+
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  # Containing environment is the global environment
+  # Superassignment operator is us
+  
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
 
+# This function computes the inverse of the given matrix
+# If the inverse of the matrix is already availble in the scope,
+# in the global scope, in this asignment, the cached value is retrived
+# If the inverse is not cached, then it is computed and stored
+
+cachesolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
